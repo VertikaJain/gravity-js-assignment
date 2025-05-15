c08e17b75b62504d10a74f50030f4548b9cf0bee
Vertika Jain <vertikajain96@gmail.com>
2025-05-15 17:53:48 +0530

    handling edge case

--- dsa.js
+++ dsa.js
@@ -2,7 +2,9 @@
 
 // example: i/p: [10,9,2,5,3,7,101,8], o/p: 4
 
+// O(n^2)
 function LIS(array) {
+  if (array.length === 0) return 0; // edge case : when array is empty
   const newArray = Array(array.length).fill(1);
 
   for (let i = 1; i < array.length; i++) {
